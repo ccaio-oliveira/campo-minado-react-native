0c5ad2a398fcae3772945355302fe83f13b6e922
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-11-26 00:15:36 -0300

    .

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
+import Field from './src/components/Field';
 import params from './src/params';
 
 export default class App extends Component {
@@ -10,6 +11,14 @@ export default class App extends Component {
                 <Text style={styles.instructions}>
                     Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}
                 </Text>
+                <Field />
+                <Field opened/>
+                <Field opened nearMines={1}/>
+                <Field opened nearMines={2}/>
+                <Field opened nearMines={3}/>
+                <Field opened nearMines={6}/>
+                <Field mined opened/>
+                <Field mined opened exploded />
             </View>
         );
     }
