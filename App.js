0c880bdd1d6d991ecea3c41616f202d2cca8d4cf
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-12-07 23:18:06 -0300

    feat: continua config do campo de minas

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import { Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
 import Field from './src/components/Field';
+import MineField from './src/components/MineField';
+import { createMinedBoard } from './src/functions';
 import params from './src/params';
 
 export default class App extends Component {
@@ -11,16 +13,7 @@ export default class App extends Component {
                 <Text style={styles.instructions}>
                     Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}
                 </Text>
-                <Field />
-                <Field opened/>
-                <Field opened nearMines={1}/>
-                <Field opened nearMines={2}/>
-                <Field opened nearMines={3}/>
-                <Field opened nearMines={6}/>
-                <Field mined opened/>
-                <Field mined opened exploded />
-                <Field flagged />
-                <Field flagged opened />
+                <MineField board={createMinedBoard(params.getRowsAmount(), params.getColumnsAmount())} />
             </View>
         );
     }
